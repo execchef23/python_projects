65ff219b2629040741d000be8dbc4ec00da48f61
Michael Manley <michaelmanley@Michaels-MBP.lan>
2022-11-22 21:30:26 -0500

    update 1

--- functions_practice.py
+++ functions_practice.py
@@ -3,3 +3,14 @@ def hello():
 
 def pack(a,b,c):
     return [a,b,c]
+
+def eat_lunch(my_lst):
+    if len(my_lst) == 0:
+        print("My lunchbox is empty!")
+    else:
+        for i in range(len(my_lst)):
+            if i == 0:
+                print(f"First I eat {my_lst[0]}")
+            else:
+                print(f"Next I eat {my_lst[i]}")
+
