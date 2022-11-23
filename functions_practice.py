e27966e2518474f6c0538a52d04cc8ac780ef20f
Michael Manley <michaelmanley@Michaels-MBP.lan>
2022-11-22 21:39:59 -0500

    added comments

--- functions_practice.py
+++ functions_practice.py
@@ -1,8 +1,16 @@
+# A function named hello() that prints a greeting to the user. This function should accept no arguments and returns nothing.
+
 def hello():
     print("Hello, user!")
 
+# A function named pack() that accepts three arguments, and return a single list with the three arguments inside a list elements.
+
 def pack(a,b,c):
     return [a,b,c]
+
+# A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __"
+# (the first element of the list), and "Next I eat __" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!".
+# The function should not return anything.
 
 def eat_lunch(my_lst):
     if len(my_lst) == 0:
@@ -14,3 +22,9 @@ def eat_lunch(my_lst):
             else:
                 print(f"Next I eat {my_lst[i]}")
 
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
